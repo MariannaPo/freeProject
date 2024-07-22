8d03bb2a108152e48e2df69e74d56dd579506b4f
Marianna_Po <mapodmasko@gmail.com>
2024-07-22 20:56:42 +0300

    correct and done

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -6,17 +6,23 @@ import s from './counter.style.module.css'
 export const Counter=()=>{
 
     const [count, setCount] = useState<number>(0);
-    const [inputValue, setInputValue] = useState<any>()
+    const [inputValue, setInputValue] = useState<any>();
+    const [inputMaxValue, setInputMaxValue] = useState<any>();
+    const [disableButton, setDisableButton] = useState<boolean>(false);
 
 
 
 const addButton =()=>{
-    setInputValue(setCount(count+1))
+    const newCount = count + 1;
+    setCount(newCount);
+    if (newCount >= inputMaxValue) {
+        setDisableButton(true);
+    }
     
 }
-
 const resetButton=()=>{
-    setCount(0)
+    setCount(0);
+    setDisableButton(false)
 }
 
 const inpValue=(event: ChangeEvent<HTMLInputElement>)=>{
@@ -25,20 +31,34 @@ const inpValue=(event: ChangeEvent<HTMLInputElement>)=>{
     console.log(value)
 }
 
+const inpMaxValue=(event: ChangeEvent<HTMLInputElement>)=>{
+    const value =  parseInt(event.currentTarget.value)
+    setInputMaxValue(value)
+     console.log(value)
+ }
 
+
+ const startButtonHandler=()=>{
+    if(inputValue >= inputMaxValue){
+        setDisableButton(true)
+        alert('"Максимальное число должно быть больше или равно текущему значению счетчика.')
+    }else{
+        setCount(inputValue)
+    }
+ }
     return(
         <>
         <div>
             <div>{count}</div>
-            <button onClick={addButton}>add</button>
+            <button onClick={addButton} disabled={disableButton}>add</button>
             <button  onClick={resetButton}>reset</button>
         </div>
         <div style={{paddingTop: '45px'}}>
          <input type='text' placeholder='Start value'              
             onChange={inpValue}
 />
-          <input type='text' placeholder='Max value'/>
-          <button>start</button>
+          <input type='text' placeholder='Max value' onChange={inpMaxValue}/>
+          <button onClick={startButtonHandler} disabled={disableButton}>start</button>
         </div>
         </>
         
